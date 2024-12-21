32b43180c98c226993b3127ff96a9393375f1293
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-21 10:14:05 +0700

    ADD interface on ContainerApp

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,16 @@
 package com.example.ucp2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2.data.database.RsDatabase
+import com.example.ucp2.repository.LocalRepositoryDok
+import com.example.ucp2.repository.RepositoryDok
+
+interface ContainerApp {
+    val repositoryDok : RepositoryDok
+}
+
+class ContainerApp(private val context: Context) : ContainerApp {
+    override val repositoryMhs: RepositoryDok by lazy {
+        LocalRepositoryDok(RsDatabase.getDatabase(context).mahasiswaDao())
+    }
+}
