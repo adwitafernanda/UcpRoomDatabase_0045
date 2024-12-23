018e640c662f9103e523e8da1061d29f2f9bbb34
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-24 04:42:43 +0700

    ADD - ContainerApp

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -2,15 +2,25 @@ package com.example.ucp2.dependenciesinjection
 
 import android.content.Context
 import com.example.ucp2.data.database.RsDatabase
-import com.example.ucp2.repository.LocalRepositoryDok
-import com.example.ucp2.repository.RepositoryDok
+import com.example.ucp2.repository.LocalRepositoryDokter
+import com.example.ucp2.repository.LocalRepositoryJadwal
+import com.example.ucp2.repository.RepositoryDokter
+import com.example.ucp2.repository.RepositoryJadwal
 
-interface ContainerApp {
-    val repositoryDok : RepositoryDok
+
+interface InterfaceContainerApp {
+    val repositorydokter: RepositoryDokter
+    val repositoryjadwal: RepositoryJadwal
 }
 
-class ContainerApp(private val context: Context) : ContainerApp {
-    override val repositoryMhs: RepositoryDok by lazy {
-        LocalRepositoryDok(RsDatabase.getDatabase(context).mahasiswaDao())
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositorydokter: RepositoryDokter by lazy {
+        LocalRepositoryDokter(RsDatabase.getDatabase(context).dokterDao())
+    }
+
+
+    override val repositoryjadwal: RepositoryJadwal by lazy {
+        LocalRepositoryJadwal(RsDatabase.getDatabase(context).jadwalDao())
     }
 }
+
